48ccad5afc02cb6cbc838c0470941f9ea9d78853
Guilherme Silva Lisboa <glisboa@samsaodev.com>
2016-11-23 00:46:35 -0500

    create scheduler

---
+++ messageScheduler.js
@@ -0,0 +1,24 @@
+
+
+var schedule = require('node-schedule');
+const Slapp = require('slapp')
+
+function Scheduler () { }
+
+/*
+    schedule a recurrent callback for weekdays (array of integers with 0 as sunday to 6 as saturday) and hour (0 to 24)
+*/
+Scheduler.prototype.scheduleCallback = function (weekdays,hour,callback) {
+    var rule = new schedule.RecurrenceRule();
+    rule.dayOfWeek = weekdays
+    rule.hour = hour
+
+    var j = schedule.scheduleJob(rule, function(){
+        callback();
+    });
+}
+
+module.exports = Scheduler;
+
+
+
